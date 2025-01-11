8ee41b967a37445ef65b5057ed804670992fe668
George S <giorgisepiskveradze@gmail.com>
2025-01-11 20:32:45 +0400

    Translation for sign up page

--- src/app/pages/authentication/sign-up/sign-up.component.ts
+++ src/app/pages/authentication/sign-up/sign-up.component.ts
@@ -7,34 +7,70 @@ import { RouterLink, Router } from '@angular/router';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { NgIf } from '@angular/common';
 import { FeathericonsModule } from '../../../icons/feathericons/feathericons.module';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-sign-up',
     standalone: true,
-    imports: [RouterLink, MatButton, MatIconButton, FormsModule, MatFormFieldModule, MatInputModule, FeathericonsModule, MatCheckboxModule, ReactiveFormsModule, NgIf],
+    imports: [RouterLink, 
+        MatButton, 
+        MatIconButton,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FeathericonsModule,
+        MatCheckboxModule,
+        ReactiveFormsModule,
+        NgIf,
+        TranslateModule],
     templateUrl: './sign-up.component.html',
     styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+    signUpForm: FormGroup;
 
     constructor(
         private fb: FormBuilder,
         private router: Router,
+        private translate: TranslateService,
     ) {
-        this.authForm = this.fb.group({
-            name: ['', Validators.required],
+        this.signUpForm = this.createAuthForm();
+    }
+
+    private passwordMatchValidator(control: FormGroup) {
+        const password = control.get('password')?.value;
+        const confirmPassword = control.get('confirmPassword')?.value;
+
+        if (password !== confirmPassword) {
+            control.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+            return { passwordMismatch: true };
+        }
+        return null;
+    }
+
+    private createAuthForm(): FormGroup {
+        return this.fb.group({
             email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(8)]],
+            firstName: ['', [Validators.required, Validators.minLength(2)]],
+            lastName: ['', [Validators.required, Validators.minLength(2)]],
+            password: ['', [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).+$')
+            ]],
+            confirmPassword: ['', [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).+$'),
+                this.passwordMatchValidator
+            ]],
         });
     }
 
-    // Password Hide
     hide = true;
 
-    // Form
-    authForm: FormGroup;
     onSubmit() {
-        if (this.authForm.valid) {
+        if (this.signUpForm.valid) {
             this.router.navigate(['/']);
         } else {
             console.log('Form is invalid. Please check the fields.');
